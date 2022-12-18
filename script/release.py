08da369f0a8526bb84c4ecee234b1df700c32c73
istinnstudio <36081997+istinnstudio@users.noreply.github.com>
2022-12-18 18:42:26 +0200

    Update release.py

--- script/release.py
+++ script/release.py
@@ -19,17 +19,17 @@ def main():
   headers = common.github_headers()
 
   try:
-    resp = urllib.request.urlopen(urllib.request.Request('https://api.github.com/repos/HumbleUI/SkiaBuild/releases/tags/' + version, headers=headers)).read()
-  except urllib.error.URLError as e:
-    data = '{"tag_name":"' + version + '","name":"' + version + '"}'
-    resp = urllib.request.urlopen(urllib.request.Request('https://api.github.com/repos/HumbleUI/SkiaBuild/releases', data=data.encode('utf-8'), headers=headers)).read()
+#   resp = urllib.request.urlopen(urllib.request.Request('https://api.github.com/repos/HumbleUI/SkiaBuild/releases/tags/' + version, headers=headers)).read()
+#  except urllib.error.URLError as e:
+#    data = '{"tag_name":"' + version + '","name":"' + version + '"}'
+#    resp = urllib.request.urlopen(urllib.request.Request('https://api.github.com/repos/HumbleUI/SkiaBuild/releases', data=data.encode('utf-8'), headers=headers)).read()
  # upload_url = re.match('https://.*/assets', json.loads(resp.decode('utf-8'))['upload_url']).group(0)
 
  # print('Uploading', zip, 'to', upload_url)
-  headers['Content-Type'] = 'application/zip'
-  headers['Content-Length'] = os.path.getsize(zip)
-  with open(zip, 'rb') as data:
-    urllib.request.urlopen(urllib.request.Request(upload_url + '?name=' + zip, data=data, headers=headers))
+#  headers['Content-Type'] = 'application/zip'
+#  headers['Content-Length'] = os.path.getsize(zip)
+ # with open(zip, 'rb') as data:
+ #   urllib.request.urlopen(urllib.request.Request(upload_url + '?name=' + zip, data=data, headers=headers))
 
   return 0
 
